d4afc3cddd694c0936aa1ebf321d14a0884bc4b9
Abdelhak Lallouche <abdlhaklalouche@gmail.com>
2019-04-05 23:26:57 +0100

    Fixing display message after install

--- src/ReactWithLoaderPresetServiceProvider.php
+++ src/ReactWithLoaderPresetServiceProvider.php
@@ -10,9 +10,9 @@ class ReactWithLoaderPresetServiceProvider extends ServiceProvider
     public function boot()
     {
         PresetCommand::macro('react-with-loader', function ($command) {
-            ReactWithLoaderPreset::install();
             $command->info('React with loader preset installed successfully.');
             $command->info('Please run "npm install" to install all required dependencies and run "npm run watch" to start developing.');
+            ReactWithLoaderPreset::install();
         });
     }
 }
